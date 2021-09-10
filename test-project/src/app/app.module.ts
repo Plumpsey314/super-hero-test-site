d90cf1994e053929d442646f79e3cfe7d6efd749
Plumpsey314 <nderhake2022@chadwickschool.org>
2021-09-09 22:53:37 -0700

    feat: create new component for test-project

--- test-project/src/app/app.module.ts
+++ test-project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SuperHeroesComponent } from './super-hero-component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuperHeroesComponent
   ],
   imports: [
     BrowserModule,
---
+++ test-project/src/app/super-hero-component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+//render this component with these properties
+@Component({
+    //css element
+    selector: 'heroes',
+
+    //html template
+    template: '<strong>components</strong>'
+})
+
+export class SuperHeroesComponent{
+
+}
